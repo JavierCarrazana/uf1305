4d8ec38a9829869954bd2089e1bc7ec77c387278
JavierCarrazana <javier.carrazana.blanco@gmail.com>
2022-03-17 13:15:27 +0100

    asd

--- js/calculos.js
+++ js/calculos.js
@@ -1,3 +1,6 @@
+// Definir variables
+//var n = 0;
+
 // Funcion para leer numeros Reales
 function leerNumero() {
     num = parseInt(
@@ -6,16 +9,52 @@ function leerNumero() {
     return num;
 }
 
-function mostrarResultados() {
-    document.getElementById('td1').innerHTML = n;
-    document.getElementById('td2').innerHTML = factorial();
+function mostrarFactorial(nf) {
+    document.getElementById('td21').innerHTML = nf;
+    document.getElementById('td22').innerHTML = factorial(nf);
 }
 
-function factorial() {
+function factorial(nf) {
     var factorial = 1;
-    for ( i=1; i <= n; i++ ) {
+    for (i = 1; i <= nf; i++) {
         factorial = factorial * i;
     }
 
     return factorial;
+}
+
+function multiplosdehasta(n, m) {
+    mensaje = "Los múltiplos de " + n + " hasta " + m + " son: "
+    multiplo = 0;
+    for (i = 1; multiplo < m; i++) {
+        multiplo = n * i
+        mensaje = mensaje + multiplo + " ";
+    }
+
+    return mensaje
+}
+
+// no estamos usando esta función
+// no hace falta que la comentemos, si no se usa no se ejecuta
+function mostrarMultiplo(n, m) {
+    document.getElementById("td11").innerHTML = n;
+    document.getElementById("td12").innerHTML = m;
+    document.getElementById("td13").innerHTML = multiplosdehasta(n, m);
+}
+
+
+function mostrarResultados(op, n1, n2 = 0) {
+
+    switch (op) {
+        case "multiplo":
+            document.getElementById("td11").innerHTML = n1;
+            document.getElementById("td12").innerHTML = n2;
+            document.getElementById("td13").innerHTML = multiplosdehasta(n1, n2);
+            break;
+        case "factorial":
+            document.getElementById('td21').innerHTML = n1;
+            document.getElementById('td22').innerHTML = factorial(n1);
+            break;
+        default:
+    }
 }
